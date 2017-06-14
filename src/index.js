b37a57fdc9a2b9eb6ec063f40030402d24ea0d9a
Kai Curry <kai@cape.io>
2017-06-13 20:27:53 -0400

    track window size

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import createStore from './createStore'
+import { getWindowAction } from './screen/actions'
 
 const props = {
   splashPage: { adminLoggedIn: true },
 }
 const store = createStore(props)
+
+window.addEventListener('resize', () => store.dispatch(getWindowAction()))
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
